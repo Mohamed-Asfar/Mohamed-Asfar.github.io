8f4056609059bbedf0cc272cbf0de82a5dd30e58
Mohamed-Asfar <79734691+Mohamed-Asfar@users.noreply.github.com>
2021-09-24 16:15:08 -0400

    n

--- itis3135/scripts/swi.js
+++ itis3135/scripts/swi.js
@@ -5,9 +5,5 @@ var condition = prompt("How are you doing", "Good, Great, etc.");
 }
 function todayDate(){
     var today = new Date();
-    document.getElementById("temp").innerHTML = date.getMonth() + 1;
-    document.getElementById("temp").innerHTML = "/"
-    document.getElementById("temp").innerHTML = date.getDate();
-    document.getElementById("temp").innerHTML = "/"
-    document.getElementById("temp").innerHTML = date.getYear();
+    today.toDateString();
 }
