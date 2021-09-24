7884ea6431655618aec661610c0e07c8c3448019
Mohamed-Asfar <79734691+Mohamed-Asfar@users.noreply.github.com>
2021-09-24 18:26:42 -0400

    s

--- itis3135/scripts/swi.js
+++ itis3135/scripts/swi.js
@@ -1,18 +1,23 @@
 
+//Asks for name
 function nameFunc(){
 var name1 = prompt("Please enter your name here", "John Smith");
-document.write(name1);
+    document.write(name1);
 }
+//Asks for mood
 function moodFunc(){
 var condition = prompt("How are you doing", "Good, Great, etc.");
-document.write(condition);
+    document.write(condition);
 }
-
+//Displays date
 function todayDate(){
-    var today = new Date();
+var today = new Date();
     document.write(today.toDateString());
 }
+//Displays time
 function todayTime(){
-    var time = new Date();
+var time = new Date();
     document.write(time.toTimeString());
 }
+
+//5 buttons for company
