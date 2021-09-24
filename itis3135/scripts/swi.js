f0cab4631a29f00cb74c3fed37d84835c751515c
Mohamed-Asfar <79734691+Mohamed-Asfar@users.noreply.github.com>
2021-09-24 16:11:37 -0400

    g

--- itis3135/scripts/swi.js
+++ itis3135/scripts/swi.js
@@ -2,5 +2,12 @@
 function mainFunc(){
 var name = prompt("Please enter your name here", "John Smith");
 var condition = prompt("How are you doing", "Good, Great, etc.");
-var today = new Date();
+}
+function todayDate(){
+    var today = new Date();
+    document.getElementById("temp").innerHTML = date.getMonth() + 1;
+    document.getElementById("temp").innerHTML = "/"
+    document.getElementById("temp").innerHTML = date.getDate();
+    document.getElementById("temp").innerHTML = "/"
+    document.getElementById("temp").innerHTML = date.getYear();
 }
