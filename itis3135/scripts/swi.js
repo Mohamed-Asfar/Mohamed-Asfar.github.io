3fa775cf32443d6b19974cb6c61643cff898f674
Mohamed-Asfar <79734691+Mohamed-Asfar@users.noreply.github.com>
2021-09-24 16:31:13 -0400

    d

--- itis3135/scripts/swi.js
+++ itis3135/scripts/swi.js
@@ -1,9 +1,13 @@
 
 function mainFunc(){
-var name = prompt("Please enter your name here", "John Smith");
+var name1 = prompt("Please enter your name here", "John Smith");
 var condition = prompt("How are you doing", "Good, Great, etc.");
 }
 function todayDate(){
     var today = new Date();
     document.write(today.toDateString());
+}
+function todayTime(){
+    var time = new Date();
+    document.write(time.toTimeString());
 }
