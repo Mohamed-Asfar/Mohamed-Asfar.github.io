68e35bc350613582a6cb6dc02e53813b698e81bc
Mohamed-Asfar <79734691+Mohamed-Asfar@users.noreply.github.com>
2021-09-24 15:56:39 -0400

    n

--- itis3135/scripts/swi.js
+++ itis3135/scripts/swi.js
@@ -2,5 +2,5 @@
 function mainFunc(){
 var name = prompt("Please enter your name here", "John Smith");
 var condition = prompt("How are you doing", "Good, Great, etc.");
-var month = new Date();
+const date = new Date();
 }
