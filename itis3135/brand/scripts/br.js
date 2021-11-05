751d8b66324a5a1f2f65b249096a717f1f49184c
Mohamed-Asfar <79734691+Mohamed-Asfar@users.noreply.github.com>
2021-11-05 15:49:30 -0400

    k

---
+++ itis3135/brand/scripts/br.js
@@ -0,0 +1,11 @@
+$(function() {
+    $("#header").load("components/header.html"); 
+    $("#footer").load("components/footer.html");
+    $("#nav").load("components/nav.html");  
+  });
+
+$(document).ready(function(){
+$("form").submit(function(){
+alert(fname, number, email, year);
+});
+});
