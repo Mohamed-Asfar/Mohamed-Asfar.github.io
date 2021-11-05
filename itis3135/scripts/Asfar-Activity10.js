2fd71e6edf2ca8646f4b9afba1f0c13cd8877615
Mohamed-Asfar <79734691+Mohamed-Asfar@users.noreply.github.com>
2021-11-04 23:45:27 -0400

    f

--- itis3135/scripts/Asfar-Activity10.js
+++ itis3135/scripts/Asfar-Activity10.js
@@ -2,8 +2,25 @@ $(document).ready(function() {
 
 
         // preload the image for each link
+        if(document.images) {
+                img1 = new Image();
+                img1.src = "h1.jpg";
+                img2 = new Image();
+                img2.src = "h2.jpg";
+                img3 = new Image();
+                img3.src = "h3.jpg";
+                img4 = new Image();
+                img4.src = "h4.jpg";
+                img5 = new Image();
+                img5.src = "h5.jpg";
+                img6 = new Image();
+                img6.src = "h6.jpg";
 
+        }
         // set up the event handlers for each link
+        $("#h1").hide().fadeIn(3000);
+        $("#li").hide().fadeIn(3000);
+        $("#p").hide().fadeIn(3000);
 
     			// get the image URL and caption for each image and animate the caption
 
