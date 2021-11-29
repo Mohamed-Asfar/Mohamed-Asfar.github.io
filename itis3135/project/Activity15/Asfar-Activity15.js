e363d70d6806b49e2f1348d58f312bf5ced76422
Mohamed-Asfar <79734691+Mohamed-Asfar@users.noreply.github.com>
2021-11-28 20:52:12 -0500

    c

--- itis3135/project/Activity15/Asfar-Activity15.js
+++ itis3135/project/Activity15/Asfar-Activity15.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
 
+    $.getJSON("faculty.json", function(data) {
+        $.each(data, function() {
+        $.each(this, function(key, value) {
+        $("#faculty").append(
+            "<img src="+"'"+value.image+"'><br>"+
+        "<h2>" + value.full_name + "</h2>" +
+        "<h3>" + value.department + "</h3>" +
+        "<p>" + value.bio + "</p><br>"
+        );
+        });
+        });
+        });
+
 });
